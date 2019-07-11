2f1ae894c60f63432f45590aedad078998c5290b
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-11 02:24:46 -0300

    Wrapper que define tamanho e envolve tela de login

---
+++ src/components/login-wrapper/index.js
@@ -0,0 +1,80 @@
+const loginWrapper = (function() {
+  const module = {};
+
+  module.children = () => {
+    const $coloredBox = coloredBox.render({
+      color: "-black",
+      variante: "-first"
+    });
+    const $secondColoredBox = coloredBox.render({
+      color: "-red",
+      variante: "-second"
+    });
+
+    const $headerCollabcode = headerCollabcode.render();
+    const $titleCollabcode = titleCollabcode.render("Bem Vindo");
+    const $formLogin = formLogin.render();
+
+    return `
+      ${$headerCollabcode}
+      ${$coloredBox}
+      ${$secondColoredBox}
+      ${$titleCollabcode}
+      ${$formLogin}
+    `;
+  };
+
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+
+    .login-wrapper {
+      width: 100vw;
+      height: 100vh;
+      max-width: 375px;
+      overflow-x: hidden;
+      margin-left: auto;
+      margin-right: auto;
+      position: relative;
+      background-color: #e5e5e5;
+      border: 1px #3A4042 solid;
+      z-index: 0;
+    }
+
+    .login-wrapper > .colored-box.-black {
+      background-color: #3A4042;
+      
+    }
+
+    .login-wrapper > .colored-box.-red {
+      background-color: #F25A70;
+    }
+
+    .login-wrapper > .colored-box.-first {
+      top: -130px;
+      left: -125px;
+      transform: rotate(-34deg);
+    }
+
+    .login-wrapper > .colored-box.-second {
+      z-index: -2;
+      left: -71px;
+      top: -114px;
+      transform: rotate(-40deg);
+    }
+    `;
+
+    $head.insertBefore($style, null);
+  };
+
+  module.render = () => {
+    module._style();
+    return `<section class="login-wrapper">${module.children()}</section>`;
+  };
+
+  return {
+    render: module.render
+  };
+})();
