aa15950f423c9087b06a16572dec42fc2e284be3
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-11 22:30:30 -0300

    Botão das telas de login e signup

---
+++ src/components/button-collabcode/index.js
@@ -0,0 +1,40 @@
+const buttonCollabcode = (function() {
+  const module = {};
+
+  module._id = 0;
+
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+    
+      .button-collabcode.button-${module._id} {
+        width: 100%;
+        height: 40px;
+        background-color: #F25A70;
+        border-radius: 20px;
+        color: #f2f2f2;
+        font-weight: bold;
+        font-size: .8rem;
+        margin-top: 13px;
+      }
+    `;
+
+    $head.insertBefore($style, null);
+  };
+  module.render = content => {
+    module._id++;
+    module._style();
+
+    return `
+      <input class="button-collabcode button-${
+        module._id
+      }" type="submit" value="${content}">
+    `;
+  };
+
+  return {
+    render: module.render
+  };
+})();
