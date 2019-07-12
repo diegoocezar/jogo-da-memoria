bc237f90d7cc8e6c2a0d2b46de31f25bf4f58c60
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-11 22:46:32 -0300

    Corrigindo tamanho do elemento

--- src/objects/text-collabcode/index.js
+++ src/objects/text-collabcode/index.js
@@ -13,6 +13,9 @@ const textCollabcode = (function() {
         font-size: .8rem;
         text-decoration: none;
         color: #000;
+        margin-right: auto;
+        margin-left: auto;
+        width: 40%;
       }
 
       .text-collabcode:hover {
