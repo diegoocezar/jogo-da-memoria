5a2745a6174c1f65c2ff04dcf88250972d012724
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-11 22:30:50 -0300

    Elementos do form adicionados

--- src/components/form-login/index.js
+++ src/components/form-login/index.js
@@ -2,13 +2,32 @@ const formLogin = (function() {
   const module = {};
 
   module.children = () => {
-    const $inputCollabcode = inputCollabcode.render({
+    const $emailLogin = inputCollabcode.render({
       type: "email",
       placeholder: "diego.cezar@exemplo.com"
     });
 
+    const $senhaLogin = inputCollabcode.render({
+      type: "password",
+      placeholder: "Digite sua senha"
+    });
+
+    const $esqueceuSenha = textCollabcode.render({
+      content: "Esqueceu a senha?"
+    });
+
+    const $botaoLogin = buttonCollabcode.render("Entrar");
+
+    const $criarConta = textCollabcode.render({
+      content: "Criar conta grátis"
+    });
+
     return `
-      ${$inputCollabcode}
+      ${$emailLogin}
+      ${$senhaLogin}
+      ${$esqueceuSenha}
+      ${$botaoLogin}
+      ${$criarConta}
     `;
   };
 
@@ -20,6 +39,16 @@ const formLogin = (function() {
       .form-login {
         margin-top: 40px;
         padding: 0 10px 10px;
+      }
+
+      .form-login > .input-collabcode:first-child {
+        margin-bottom: 43px;
+      }
+
+      .form-login > .text-collabcode {
+        text-align: center;
+        margin-top: 11px;
+        margin-bottom: 11px;
       }
 
     `;
