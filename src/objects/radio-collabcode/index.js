d93d11eb16cfb06f8d1ba6f163c260eceae5a2c8
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-16 01:37:38 -0300

    Radio button para seleção de nível

---
+++ src/objects/radio-collabcode/index.js
@@ -0,0 +1,40 @@
+const radioCollabcode = (function() {
+  const module = {};
+
+  module._id = 0;
+
+  // module._style = () => {
+  //   const $head = document.querySelector("head");
+  //   const $style = document.createElement("style");
+
+  //   $style.textContent = `
+  //     .radio-collabcode.radio-${module._id} {
+
+  //     }
+  //   `;
+
+  //   $head.insertBefore($style, null);
+  // };
+
+  module.handleClick = component => {
+    store.dificuldade = component.value;
+  };
+
+  module.render = ({ dificuldade }) => {
+    module._id++;
+    // console.log(dificuldade);
+    return `
+      <input 
+      class="radio-collabcode radio-${module._id}" 
+      type="radio" name="dificuldade" 
+      id="${dificuldade}"
+      onclick="radioCollabcode.handleClick(this)"
+      value="${dificuldade}">
+    `;
+  };
+
+  return {
+    render: module.render,
+    handleClick: module.handleClick
+  };
+})();
