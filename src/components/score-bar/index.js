6069fcdb1733116cf1991f7ea7c5e010d2e84411
Diego Cezar <diegocezar.comercial@gmail.com>
2019-08-01 22:35:54 -0300

    criados - game page, botões, layer e score bar

--- src/components/score-bar/index.js
+++ src/components/score-bar/index.js
@@ -15,13 +15,21 @@ const scoreBar = (function() {
     
         text-align: right;
         font-family: 'Comfortaa', sans-serif;
+        display: flex;
+        justify-content: flex-end;
       }
 
-      .score-bar > .score {
+      .score-bar > .score, 
+      .score-bar > .timer-collabcode {
         line-height: 45px;
         color: #fff;
         font-weight: bold;
         margin-right:10px;
+      }
+
+      .score-bar > .timer-collabcode {
+        margin-right: 20px;
+
       }
     `;
 
@@ -33,10 +41,54 @@ const scoreBar = (function() {
     $score.textContent = `Placar: ${store.score} pontos`;
   };
 
+  module._children = () => {
+    const $timerCollabcode = timerCollabcode.render();
+
+    const $pauseButton = gameButton.render({
+      buttonClass: "-pause",
+      content: "pausar",
+      path: "pause"
+    });
+
+    return `
+      ${$timerCollabcode}
+      ${$pauseButton}
+   `;
+  };
+
+  module.timer = seconds => {
+    const $timer = document.querySelector(".timer-collabcode");
+    const $pauseButton = document.querySelector(".-pause");
+    const minutos = 0;
+    let segundos = seconds;
+    const counter = setInterval(() => {
+      if ($pauseButton.classList.contains("-paused")) {
+        clearInterval(counter);
+        store.tempoPausado = segundos;
+        console.log("pausei em:", store.tempoPausado);
+      } else {
+        if (minutos == 0 && segundos >= 0) {
+          $timer.textContent = `Tempo: 0${minutos}:${segundos}`;
+          if (segundos < 10) {
+            $timer.textContent = `Tempo: 0${minutos}:0${segundos}`;
+          }
+
+          segundos = segundos - 1;
+        }
+      }
+    }, 1000);
+
+    if (segundos == 0) {
+      clearInterval(counter);
+    }
+  };
+
   module.create = () => {
     module._style();
     return `
      <header class="score-bar">
+     
+      ${module._children()}
       <span class="score">Placar: 0 pontos</span>
      </header>
     `;
@@ -44,6 +96,7 @@ const scoreBar = (function() {
 
   return {
     create: module.create,
-    score: module.updateScore
+    score: module.updateScore,
+    timer: module.timer
   };
 })();
--- src/global/store.js
+++ src/global/store.js
@@ -2,5 +2,6 @@ const store = {
   score: 0,
   qtdMemoryCardActive: 0,
   cardsClicados: [],
-  imgCardsClicados: []
+  imgCardsClicados: [],
+  tempoPausado: 0
 };
